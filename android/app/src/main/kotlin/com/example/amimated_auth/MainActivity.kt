4c149d0fcb467b07b43f173399540c96876d0eda
shivam.gupta9 <kishang.90@gmail.com>
2024-07-15 13:38:49 +0530

    initial

---
+++ android/app/src/main/kotlin/com/example/amimated_auth/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.amimated_auth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/views/auth_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../controller/auth_controller.dart';
+import '../widgets/login_widget.dart';
+import '../widgets/register_widget.dart';
+import '../widgets/top_overlay.dart';
+
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
+      body: Center(
+        child: Stack(
+          children: [
+            Container(
+                height: size.height * 0.8,
+                width: size.width * 0.8,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.white,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    left: size.width * 0.05,
+                    right: size.width * 0.05,
+                  ),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Consumer<AuthController>(
+                            builder: (context, authController, child) {
+                              return RegisterWidget(
+                                size: size,
+                                isLogin: authController.isLogin,
+                                authController: authController,
+                              );
+                            }),
+                        Consumer<AuthController>(
+                            builder: (context, authController, child) {
+                              return LoginWidget(
+                                size: size,
+                                isLogin: authController.isLogin,
+                                authController: authController,
+                              );
+                            }),
+                      ]),
+                )),
+            TopOverlayWidget(size: size),
+          ],
+        ),
+      ),
+    );
+  }
+}
